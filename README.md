c6ec03dd7c7351f3c11eba811c9db19b6573407d
Freddyy <alfredboak@gmail.com>
2023-09-07 20:54:24 +0100

    Question Added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Gurugeeks_Jquery_Test
+
---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  //1.write a funtion to  Add Task
+
+
+
+
+
+
+
+  //2.write a function to Delete Task
+
+
+
+
+
+  
+});
